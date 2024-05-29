4c25326a35310a44a060e0d5a9d02ae7e99216bb
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-05-29 23:26:11 +0545

    front booking table

--- lakeside-Hotel_Front/lakeside-hotel/src/components/bookings/BookingsTable.jsx
+++ lakeside-Hotel_Front/lakeside-hotel/src/components/bookings/BookingsTable.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const BookingsTable = () => {
+const BookingsTable = ({bookingInfo, handleBookingCancellation}) => {
+    const[filterdBookings, setFilteredBookings] = useState(bookingInfo)
+
+    const filterBookings = (startDate, endDate) =>{
+        let filtered = bookingInfo
+        if(startDate && endDate){
+            filtered = bookingInfo.filter((booking) =>{
+                const bookingStartDate = parseISO(booking.chekInDate)
+                const bookingEndDate = parseISO(booking.chekOutDate)
+                return bookingStartDate >= startDate && bookingEndDate <= endDate && bookingEndDate > startDate
+                
+            })
+
+        }
+
+
+    }
+
   return (
     <div>BookingsTable</div>
   )
