b954e12c0e23192ef121ef142eec4054887ce92b
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-05-31 22:57:53 +0545

    front: Bookings

--- lakeside-Hotel_Front/lakeside-hotel/src/App.jsx
+++ lakeside-Hotel_Front/lakeside-hotel/src/App.jsx
@@ -14,6 +14,7 @@ import RoomListing from './components/room/RoomListing.jsx';
 import Admin from './components/admin/Admin.jsx';
 import Checkout from './components/bookings/Checkout.jsx';
 import BookingSuccess from './components/bookings/BookingSuccess.jsx';
+import Bookings from './components/bookings/Bookings.jsx';
 
 
 function App() {
@@ -31,6 +32,8 @@ function App() {
           <Route path="/browse-all-rooms" element={<RoomListing />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/booking-success" element={<BookingSuccess />} />
+          <Route path="/existing-bookings" element={<Bookings />} />
+
 
 
         </Routes>
