dbed377ad7e6de3d762b06f7e57421adc956a524
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-07-24 16:56:26 +0545

    "back : role "

--- lakeside-Hotel/src/main/java/com/lakesidedemo/lakesideHotel/model/Role.java
+++ lakeside-Hotel/src/main/java/com/lakesidedemo/lakesideHotel/model/Role.java
@@ -13,37 +13,66 @@ import java.util.List;
  * @author : rabin
  */
 
+/**
+ * Entity class representing a Role.
+ *
+ * This class is annotated with JPA annotations to map it to a database table.
+ * It also uses Lombok annotations to generate getters, setters, and a no-argument constructor.
+ */
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class Role {
+
+    // Primary key for the Role entity
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // Name of the role
     private String name;
+
+    // Many-to-many relationship with User
     @ManyToMany(mappedBy = "roles")
     private Collection<User> users = new HashSet<>();
 
-    public void assignRoleToUser(User user){
+    /**
+     * Assigns this role to a user.
+     *
+     * @param user The user to assign the role to.
+     */
+    public void assignRoleToUser(User user) {
         user.getRoles().add(this);
         this.getUsers().add(user);
     }
 
-    public void removeUserFromRole(User user){
+    /**
+     * Removes this role from a user.
+     *
+     * @param user The user to remove the role from.
+     */
+    public void removeUserFromRole(User user) {
         user.getRoles().remove(this);
         this.getUsers().remove(user);
     }
 
-    public void removeAllUsersFromRole(){
-        if(this.getUsers() != null){
+    /**
+     * Removes this role from all users.
+     */
+    public void removeAllUsersFromRole() {
+        if (this.getUsers() != null) {
             List<User> roleUsers = this.getUsers().stream().toList();
-            roleUsers.forEach(this :: removeUserFromRole);
+            roleUsers.forEach(this::removeUserFromRole);
         }
     }
 
-    public String getName(){
-        return name != null? name : "";
+    /**
+     * Gets the name of the role.
+     *
+     * @return The name of the role, or an empty string if the name is null.
+     */
+    public String getName() {
+        return name != null ? name : "";
     }
-
 }
--- lakeside-Hotel/src/main/java/com/lakesidedemo/lakesideHotel/repository/RoleRepository.java
+++ lakeside-Hotel/src/main/java/com/lakesidedemo/lakesideHotel/repository/RoleRepository.java
@@ -8,6 +8,20 @@ import java.util.Optional;
 /*
  * @author : rabin
  */
+
+/**
+ * Repository interface for Role entity.
+ *
+ * This interface extends JpaRepository to provide CRUD operations for the Role entity.
+ * It also defines a custom query method to find a Role by its name.
+ */
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    /**
+     * Finds a role by its name.
+     *
+     * @param roleUser The name of the role to find.
+     * @return An Optional containing the found role, or empty if no role is found.
+     */
     Optional<Role> findByName(String roleUser);
 }
