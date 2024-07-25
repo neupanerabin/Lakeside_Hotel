09f948adc861acf21521a6d54507f3df6992a47f
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-07-25 14:10:32 +0545

    back security

--- lakeside-Hotel/src/main/java/com/lakesidedemo/lakesideHotel/security/HotelSecurityConfig.java
+++ lakeside-Hotel/src/main/java/com/lakesidedemo/lakesideHotel/security/HotelSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Configuration class for security-related beans.
- *
  * This class defines the beans required for the security configuration of the application.
  */
 @Configuration
@@ -19,7 +18,6 @@ public class HotelSecurityConfig {
 
     /**
      * Bean definition for PasswordEncoder.
-     *
      * This bean provides a BCryptPasswordEncoder that can be used to encode passwords
      * for secure storage and comparison.
      *
